7e7066d46f4e87a0ebbec346a066c7c886152f73
Paulo McNally <paulomcnally@gmail.com>
2015-01-01 23:30:42 -0600

    set app title

--- assets/js/controllers/app.js
+++ assets/js/controllers/app.js
@@ -1,4 +1,5 @@
 app.controller('appController', function($scope, titleService) {
+  $scope.title = 'Angular App';
   $scope.$on('handleTitleBroadcast', function() {
     $scope.tttle = titleService.title;
   });
