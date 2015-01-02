18ed06aaac9a6faa2a556cfcb3598a8f49320c6c
Paulo McNally <paulomcnally@gmail.com>
2015-01-01 23:29:41 -0600

    remove spaces

--- assets/js/controllers/app.js
+++ assets/js/controllers/app.js
@@ -1,5 +1,5 @@
 app.controller('appController', function($scope, titleService) {
-    $scope.$on('handleTitleBroadcast', function() {
-      $scope.tttle = titleService.title;
-    });
+  $scope.$on('handleTitleBroadcast', function() {
+    $scope.tttle = titleService.title;
+  });
 });
